a8a342b76faf31dfb72093cc9c270c9252f05c29
hnustxxq <112087067+hnustxxq@users.noreply.github.com>
2024-07-04 15:56:09 +0800

    Delete scripts directory

--- scripts/RacketSports.sh
+++
@@ -1,1 +0,0 @@
-python -u train_class.py     --dataset RacketSports --size_cent   24   --size_1  24  --epoch  8   --epoch_1  5  --gru_dime   200  --a_3  600  --c  2  --pred_len  35   --p_recon 1  --p  0.5  --multi  70  --count  4  --port  2  --gpu 0   --seed 42  --max-threads  8
